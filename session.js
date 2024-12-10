c328aa75d58e3cf56706cde7b31c0f9bf52b7e24
ImeshBro12 <113542023+ImeshBro12@users.noreply.github.com>
2024-12-10 18:31:38 +0530

    Update session.js

--- session.js
+++ session.js
@@ -52,5 +52,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=fYclUa4L#JFYk6vcbSk6tPpeenqoRP1_EHOa_ax2VYimRI2oeV6Y",
 };
